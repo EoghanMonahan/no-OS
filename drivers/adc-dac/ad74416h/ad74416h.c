4caabfdaff38020fe618cc70db733a6fd18318c5
Eoghan Monahan <eoghan.monahan@analog.com>
2024-03-29 17:45:28 +0000

    WIP

--- drivers/adc-dac/ad74416h/ad74416h.c
+++ drivers/adc-dac/ad74416h/ad74416h.c
@@ -1080,7 +1080,7 @@ void HART_ReadRxData(struct ad74416h_desc *desc, uint16_t* hart_data_rx, uint8_t
 uint8_t HART_TxFIFO_ByteCount(struct ad74416h_desc *desc)
 {
     uint8_t totallen;
-    ad74416h_reg_read(AD74416H_HART_TFC(0), totallen);
+    ad74416h_reg_read(desc, AD74416H_HART_TFC(0), totallen);
     return totallen;
 }
 
@@ -1119,17 +1119,17 @@ void HART_Read_Single_byte_config(struct ad74416h_desc *desc)
 {
 
     ad74416h_reg_write(desc, AD74416H_ALERT_MASK, 0x7FFF);  
-    ad74416h_reg_write(desc, AD74416H_CH_FUNC_SETUP, 0x0000);          
-    ad74416h_reg_write(desc, AD74416H_CH_FUNC_SETUP, 0x000C);          //Iin Loop powered with HART Mode
+    ad74416h_reg_write(desc, AD74416H_CH_FUNC_SETUP(0), 0x0000);          
+    ad74416h_reg_write(desc, AD74416H_CH_FUNC_SETUP(0), 0x000C);          //Iin Loop powered with HART Mode
 
-    ad74416h_reg_write(desc, AD74416H_OUTPUT_CONFIG, 0x0140);
+    ad74416h_reg_write(desc, AD74416H_OUTPUT_CONFIG(0), 0x0140);
       
-    ad74416h_reg_write(desc, AD74416H_HART_ALERT_MASK, 0xFFE7);
+    ad74416h_reg_write(desc, AD74416H_HART_ALERT_MASK(0), 0xFFE7);
    
-    ad74416h_reg_write(desc, AD74416H_HART_FCR, 0x0007);             //Clear tX and Rx Fifo
-    ad74416h_reg_write(desc, AD74416H_HART_FCR, 0x0009);
+    ad74416h_reg_write(desc, AD74416H_HART_FCR(0), 0x0007);             //Clear tX and Rx Fifo
+    ad74416h_reg_write(desc, AD74416H_HART_FCR(0), 0x0009);
    
-    ad74416h_reg_write(desc, AD74416H_HART_CONFIG, 0xF031);          //enable hart modem
+    ad74416h_reg_write(desc, AD74416H_HART_CONFIG(0), 0xF031);          //enable hart modem
     
     Wait_us(1000);
     ad74416h_reg_write(desc, AD74416H_HART_FCR, 0x000F);
